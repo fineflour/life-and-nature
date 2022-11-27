67ec7525a8f84d94af98f2112f7ea4391ff82bbc
fineflour <soojin.cho@bfa.org>
2022-11-27 11:39:11 -0800

    css fixed again

--- app/assets/javascript/application.js
+++ app/assets/javascript/application.js
@@ -1,5 +1,5 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
-import * as bootstrap from "bootstrap"
-import './bootstrap'
+//= require jquery
+//= require bootstrap
+//= require_tree .
+
