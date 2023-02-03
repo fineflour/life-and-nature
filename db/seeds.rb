025093a56b5d35d03e6837ffcc5ce154c60dec8a
fineflour <soojin.cho@bfa.org>
2023-02-02 20:18:33 -0800

    fix seed file

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+
+# for seed data, look in the /db/seed folder and store your seed file(s) there
+#
+# # loads all .rb files in the seed folder
+# unless Rails.env.production?
+   Dir[File.dirname(__FILE__) + '/seed/*.rb'].each {|file| require file } 
+#   else
+#     puts "did you really try to seed production?"
+#     end
