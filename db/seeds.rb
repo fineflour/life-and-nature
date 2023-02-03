fe2f9ebf550a005e7046d6f5f07fd2a58ed0a272
fineflour <soojin.cho@bfa.org>
2023-02-02 21:09:35 -0800

    sitemap modified

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,8 @@
 # for seed data, look in the /db/seed folder and store your seed file(s) there
 #
 # # loads all .rb files in the seed folder
-# unless Rails.env.production?
-   Dir[File.dirname(__FILE__) + '/seed/*.rb'].each {|file| require file } 
-#   else
-#     puts "did you really try to seed production?"
-#     end
+unless Rails.env.production?
+  Dir[File.dirname(__FILE__) + '/seed/*.rb'].each {|file| require file } 
+else
+  puts "did you really try to seed production? please comment out in the seed file"
+end
